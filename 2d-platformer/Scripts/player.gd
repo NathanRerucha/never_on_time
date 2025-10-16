bbf92a791024bd46da93f52f6c44703832b48b09
CT-StudentPage <cthompson029@student.butte.edu>
2025-10-15 17:43:21 -0700

    Update player.gd

--- 2d-platformer/Scripts/player.gd
+++ 2d-platformer/Scripts/player.gd
@@ -26,7 +26,7 @@ func _physics_process(delta: float) -> void:
 		velocity.x = lerp(velocity.x, 0.0, braking * delta)
 	
 	#jumping
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_force
 		
 	move_and_slide()
