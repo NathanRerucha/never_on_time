3f78d99034840ca4c5464c3dbc5a1e6c399e4d9e
HBelak <hbelak002@student.butte.edu>
2025-12-03 12:08:56 -0800

    stuff

--- Scripts/ladder_1.gd
+++ Scripts/ladder_1.gd
@@ -14,3 +14,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	print("ladder")
 	if body.is_in_group("player"):
 		body.on_ladder = true
+
+
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	if body.is_in_group("player"):
+		body.on_ladder = false
+		body.is_climbing = false
--- interaction/interaction_manager.gd
+++ interaction/interaction_manager.gd
@@ -5,7 +5,7 @@ class_name interaction_manager
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var label = $Label
 
-const base_text = "[F] to "
+const base_text = ""
 
 var active_areas = []
 var can_interact = true
@@ -22,7 +22,7 @@ func unregister_area(area: interaction_area):
 func _process(_delta):
 	if active_areas.size() > 0 && can_interact:
 		active_areas.sort_custom(_sort_by_distance_to_player)
-		Label.text = base_text + active_areas[0].action_name
+		label.text = base_text + active_areas[0].action_name
 		label.global_position = active_areas[0].global_position
 		label.global_position.y -= 36
 		label.global_position.x -= label.size.x / 2
