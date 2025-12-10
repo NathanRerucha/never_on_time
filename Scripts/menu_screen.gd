0e4e1e224494c6a2e392ad90c11e9e8a28730b69
CT-StudentPage <cthompson029@student.butte.edu>
2025-12-10 11:04:52 -0800

    Splash Screen

--- Scripts/menu_screen.gd
+++ Scripts/menu_screen.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/level_1.tscn")
+	get_tree().change_scene_to_file("res://Scenes/splash_screen.tscn")
 
 
 func _on_quit_button_pressed() -> void:
