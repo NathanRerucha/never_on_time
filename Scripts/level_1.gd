101a22b4a98bf28c90548339463f0c73555b975b
NathanRerucha <nrerucha001@student.butte.edu>
2025-12-09 17:34:57 -0800

    Okay Level 1 should be working, let me know if anything is missing

--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+@onready var player = $Player
+
+var power_is_on : bool = false
+
 func _ready() -> void:
-	$Player.spawn_point = $Spawn.position
-	$Player.shift_dist = 288
+	player.shift_dist = -2859
+	
+func _on_lv_1_generator_past_power_on() -> void:
+	power_is_on = true
