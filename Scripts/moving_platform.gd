d1ec5c5cd522e9bc7070ffa2a81ab76a9fbe78c6
HBelak <hbelak002@student.butte.edu>
2025-12-09 16:48:56 -0800

    Elavator (im amazing)

--- Scripts/moving_platform.gd
+++ Scripts/moving_platform.gd
@@ -1,19 +1,32 @@
 extends Path2D
 
 @export var loop = true
-@export var speed = 2.0
+@export var speed = 4.0
 @export var speed_scale = 1.0
 
 @onready var path = $PathFollow2D
 @onready var animation = $AnimationPlayer
+@onready var Globals = $"/root/Globals"
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if not loop:
-		animation.play("move")
-		animation.speed_scale = speed_scale
-		set_process(false)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	path.progress += speed
+	path.progress += 0
+	if Globals.go_down == true:
+		path.progress += speed
+		animation.play("move")
+		animation.speed_scale = speed_scale
+		set_process(false)
+		if !animation.is_playing():
+			Globals.go_down = false
+	
+	if Globals.go_up == true:
+		path.progress += speed
+		animation.play("move back")
+		animation.speed_scale = speed_scale
+		set_process(false)
+		if !animation.is_playing():
+			Globals.go_up = false
--- Scripts/switch.gd
+++ Scripts/switch.gd
@@ -1,15 +1,15 @@
-extends Node
+extends Sprite2D
 
-var key_taken = false
-var go_down = false
-var go_up = false
-
+@onready var interaction_area: interaction_area = $InteractionArea
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	interaction_area.interact = Callable(self, "on_interact")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func on_interact():
+	Globals.go_down = true
