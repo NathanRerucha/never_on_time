fdfaf861af74bf1abe6d3d67c2094a0ade2d69f2
CT-StudentPage <cthompson029@student.butte.edu>
2025-12-09 17:38:25 -0800

    Main Menu added

--- interaction/interaction_manager.gd
+++ interaction/interaction_manager.gd
@@ -1,8 +1,7 @@
 extends Node2D
 class_name interaction_manager
 
-
-@onready var player = get_tree().get_first_node_in_group("Player")
+var player: Node2D
 @onready var label = $Label
 
 const base_text = ""
@@ -10,16 +9,20 @@ const base_text = ""
 var active_areas = []
 var can_interact = true
 
-
 func register_area(area: interaction_area):
 	active_areas.push_back(area)
 
 func unregister_area(area: interaction_area):
 	var index = active_areas.find(area)
-	if index != 1:
+	if index != -1:
 		active_areas.remove_at(index)
 
 func _process(_delta):
+	if not player:
+		player = get_tree().get_first_node_in_group("Player")
+		if not player:
+			return
+
 	if active_areas.size() > 0 && can_interact:
 		active_areas.sort_custom(_sort_by_distance_to_player)
 		label.text = base_text + active_areas[0].action_name
@@ -32,6 +35,9 @@ func _process(_delta):
 		
 
 func _sort_by_distance_to_player(area1, area2):
+	if not is_instance_valid(area1) or not is_instance_valid(area2):
+		return false
+
 	var area1_to_player = player.global_position.distance_to(area1.global_position)
 	var area2_to_player = player.global_position.distance_to(area2.global_position)
 	return area1_to_player < area2_to_player
